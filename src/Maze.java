2facb71db601b8af5c2414fccdda531f017a1077
Philip Seger <seger.philip@gmail.com>
2016-04-16 14:13:50 -0400

    beginning of maze class

---
+++ src/Maze.java
@@ -0,0 +1,40 @@
+/**
+ * Created by philip on 4/16/16.
+ */
+public class Maze {
+    private int[][] maze;
+
+    /** TODO
+     * No args constructor, builds a random maze.
+     *
+     */
+    public Maze() {
+
+    }
+
+    /** TODO
+     * Constructor that takes a maze filename and generates
+     * the necessary objects for a maze.
+     * @param filename
+     */
+    public Maze(String filename) {
+
+    }
+
+    public int[][] readTextMap(String filename) {
+
+    }
+
+    public void createNewMaze() {
+
+    }
+
+    public void generateGraph() {
+
+    }
+
+
+    public static void main(String[] args) {
+        Maze mz1 = new Maze();
+    }
+}
