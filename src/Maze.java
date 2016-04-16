1531cbe86414543a119332147c8c2122d791c5eb
Philip Seger <seger.philip@gmail.com>
2016-04-16 14:34:51 -0400

    filling out structure for Maze class

--- src/Maze.java
+++ src/Maze.java
@@ -3,8 +3,9 @@
  */
 public class Maze {
     private int[][] maze;
+//    TODO create a map variable
 
-    /** TODO
+    /** TODO implement noargs Maze constructor
      * No args constructor, builds a random maze.
      *
      */
@@ -12,7 +13,7 @@ public class Maze {
 
     }
 
-    /** TODO
+    /** TODO implement Maze constructor(filename)
      * Constructor that takes a maze filename and generates
      * the necessary objects for a maze.
      * @param filename
@@ -21,19 +22,46 @@ public class Maze {
 
     }
 
-    public int[][] readTextMap(String filename) {
-
-    }
-
-    public void createNewMaze() {
-
-    }
-
+    /** TODO implement generateGraph
+     * Generates a graph from a 2D (int[][]) array.
+     */
     public void generateGraph() {
 
     }
 
+    /** TODO implement getMap
+     * Getter for map.
+     * @return
+     */
+    public int[][] getMap() {
+        return new int[1][1];
+    }
 
+    /** TODO implement setMap
+     * Setter for map.
+     * @param newMaze
+     */
+    public void setMap(int[][] newMaze) {
+
+    }
+
+    /** TODO implement getGraph
+     * Getter for graph.
+     */
+    public void getGraph() {
+
+    }
+
+    /** TODO implement setGraph
+     * Setter for graph.
+     */
+    public void setGraph() {
+
+    }
+
+    /**
+     * Main method for testing.
+     */
     public static void main(String[] args) {
         Maze mz1 = new Maze();
     }
