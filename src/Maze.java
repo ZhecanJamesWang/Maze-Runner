2377b4d0e048d3614033b34e832e8b55e733ab2b
Philip Seger <seger.philip@gmail.com>
2016-04-17 17:17:23 -0400

    able to read text file maze now

--- src/Maze.java
+++ src/Maze.java
@@ -1,3 +1,9 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import java.util.Vector;
+
 /**
  * Created by philip on 4/16/16.
  */
@@ -19,7 +25,26 @@ public class Maze {
      * @param filename
      */
     public Maze(String filename) {
+        maze = new int[15][15]; //TODO unconstrain the maze size, keeping at 15x15 for now
 
+//        load the text file
+        try {
+            Scanner scan = new Scanner(new FileReader(filename));
+
+//            save the contents of the maze file into the maze array
+            int row = 0;
+            while (scan.hasNextLine()) {
+                String line = scan.nextLine();
+
+                for (int column = 0; column < line.length(); column++) {
+                    maze[row][column] = line.charAt(column) - 48;
+                }
+                row++;
+            }
+        }
+        catch (FileNotFoundException exception) {
+            System.out.println("Unable to locate text file. Check the name again.");
+        }
     }
 
     /** TODO implement generateGraph
@@ -34,15 +59,17 @@ public class Maze {
      * @return
      */
     public int[][] getMap() {
-        return new int[1][1];
+        return maze;
     }
 
     /** TODO implement setMap
      * Setter for map.
-     * @param newMaze
+     * @param x coordinate
+     * @param y coordinate
+     * @param newVal value to replace at coords
      */
-    public void setMap(int[][] newMaze) {
-
+    public void setMap(int x, int y, int newVal) {
+        maze[x][y] = newVal;
     }
 
     /** TODO implement getGraph
@@ -59,10 +86,24 @@ public class Maze {
 
     }
 
+    public String toString() {
+        String result = "";
+        for (int i = 0; i < maze.length; i++) {
+            for (int j = 0; j < maze.length; j++) {
+                result += maze[i][j];
+            }
+            result += "\n";
+        }
+        return result;
+    }
+
     /**
      * Main method for testing.
      */
     public static void main(String[] args) {
-        Maze mz1 = new Maze();
+        Maze mz1 = new Maze("level1.txt");
+        System.out.println(mz1);
+        mz1.setMap(1, 1, 5);
+        System.out.println(mz1);
     }
 }
